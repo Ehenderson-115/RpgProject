7ffcce77addf030b67842e34060c9e0d5e45e43a
Elijah Henderson <elijah.henderson@prtobx.com>
2023-02-28 11:00:13 -0500

    Output Manager added and functional. Combat code refactored. Combat ending is not working as intended :(

---
+++ GameObjects/OutputManager.cpp
@@ -0,0 +1,46 @@
+#include "OutputManager.h"
+#include "HelperFunctions.h"
+#include "Character.h"
+#include "Player.h"
+#include "Room.h"
+#include "ActiveGameData.h"
+
+OutputManager::OutputManager()
+	: mOutput{""}
+	, mStatusBar{""}
+{}
+
+void OutputManager::UpdateStatusBar(const std::shared_ptr<ActiveGameData>& inData)
+{
+	switch (inData->mState)
+	{
+	case Game::GameState::Main:
+		mStatusBar = ("Current Location: " + inData->mRoom->Name() + "\n");
+		break;
+	case Game::GameState::Menu:
+		mStatusBar = (inData->mPlayer->GetStatus() + "\n");
+		break;
+	case Game::GameState::Combat:
+		mStatusBar = (inData->mPlayer->Character::GetStatus() + " ||| " + inData->mAdversary->GetStatus() + "\n");
+		break;
+	case Game::GameState::CombatStart:
+		mStatusBar = (inData->mPlayer->Character::GetStatus() + " ||| " + inData->mAdversary->GetStatus() + "\n");
+		break;
+	}
+}
+
+void OutputManager::AddToOutput(const std::string& inStr)
+{
+	mOutput += inStr;
+}
+
+void OutputManager::PrintScreen()
+{
+	system("cls");
+	FormattedPrint(mStatusBar);
+	if (!mOutput.empty())
+	{
+		FormattedPrint(mOutput);
+	}
+	mOutput.clear();
+}
